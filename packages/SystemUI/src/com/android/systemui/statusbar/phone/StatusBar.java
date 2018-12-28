46e8661715c8b75e5824015dc4a50cfcf5e3140d
maxwen <max.weninger@gmail.com>
2018-12-28 17:07:03 +0530

    base: QS rows & columns customization
    base: SystemUI: make QS columns count configurable
    
    And add infrastructure to add more settings and
    distribute them to QS panel
    
    PS4
    we must explicitly hide the columns submenu
    
    base: SystemUI: add column count 6
    
    now that we have hide label
    
    base: SystemUI: qs colums menu with checkboxes
    
    base: SystemUI add landscape qs columns config
    
    allow max 9 columns in landscape
    
    Change-Id: I401cca347da1a94f352a9ffcb99f1409ddc4d008
    
    base: SystemUI: add back QS hide label
    
    if enabled label click (secondary) will be reassigned to long click
    for dual target tiles
    
    Change-Id: I440362ab38057cbfba7750d75825179bd830c4e8
    
    base: SystemUI: add QS rows config
    
    with inspiration from @ezio84
    
    Change-Id: I980a1ab2ed024f467367b222ab408ccfe1e1882f
    
    base: make qs lanscape rows count configurable
    
    * based on ezio84's and omnirom's code
    
    Change-Id: I0e57eaa69c4a8f48bdb4cdc117f57f05c6a58dbe
    
    base: SystemUI: add seven and eight qs columns for portrait
    
    Change-Id: Ia7c4bc759bfa55e3d47161d41bc210e892694122
    
    base: SystemUI: qs quickbar columns config
    
    Change-Id: I11b85e6ad2a3c46fb401ec9bccd18718192e8d76
    
    base: SystemUI: add qs quick tile column auto to follow columns
    
    always use same columns as the expanded panel but
    never less then 6
    
    Change-Id: I13f5f871cc1362d1fb8c60ba07aeff9675f1c32a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -207,6 +207,7 @@ import com.android.systemui.plugins.statusbar.NotificationSwipeActionHelper.Snoo
 import com.android.systemui.qs.QSFragment;
 import com.android.systemui.qs.QSPanel;
 import com.android.systemui.qs.QSTileHost;
+import com.android.systemui.qs.QuickQSPanel;
 import com.android.systemui.qs.car.CarQSFragment;
 import com.android.systemui.recents.Recents;
 import com.android.systemui.recents.ScreenPinningRequest;
@@ -444,6 +445,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     // settings
     private QSPanel mQSPanel;
+    private QuickQSPanel mQuickQSPanel;
 
     private DisplayManager mDisplayManager;
 
@@ -1221,6 +1223,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     mQSPanel = ((QSFragment) qs).getQsPanel();
                     mQSPanel.setBrightnessMirror(mBrightnessMirrorController);
                     mKeyguardStatusBar.setQSPanel(mQSPanel);
+                    mQuickQSPanel = ((QSFragment) qs).getQuickQsPanel();
                 }
             });
         }
@@ -3920,7 +3923,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mQSPanel != null) {
             mQSPanel.updateResources();
         }
-
+        if (mQuickQSPanel != null) {
+            mQuickQSPanel.updateResources();
+        }
         loadDimens();
 
         if (mStatusBarView != null) {
