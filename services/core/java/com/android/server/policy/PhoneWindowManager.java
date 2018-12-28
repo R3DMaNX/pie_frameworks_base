omnirom/android_frameworks_base_old

[1/2] base: op gesture tweaks
Change-Id: I8dc6be8a43f76eb5e306d4347d6066e339c9f020

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1076,6 +1076,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.OMNI_LONG_PRESS_POWER_TORCH), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.OMNI_BOTTOM_GESTURE_TRIGGER_TIMEOUT), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.OMNI_BOTTOM_GESTURE_SWIPE_LIMIT), false, this,
+                    UserHandle.USER_ALL);
             updateSettings();
         }
 
@@ -2672,6 +2678,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mGestureButtonRegistered && !mUseGestureButton) {
             mWindowManagerFuncs.unregisterPointerEventListener(mGestureButton);
             mGestureButtonRegistered = false;
+        }
+        if(mUseGestureButton && mGestureButton != null) {
+            mGestureButton.updateSettings();
         }
     }
 

R3DMaNX/pie_frameworks_base

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1247,6 +1247,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.RECENTS_LAYOUT_STYLE), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.BOTTOM_GESTURE_TRIGGER_TIMEOUT), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.BOTTOM_GESTURE_SWIPE_LIMIT), false, this,
+                    UserHandle.USER_ALL);
             updateSettings();
         }
 
@@ -3288,6 +3294,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mGestureButtonRegistered && !mUseGestureButton) {
             mWindowManagerFuncs.unregisterPointerEventListener(mGestureButton);
             mGestureButtonRegistered = false;
+        }
+        if(mUseGestureButton && mGestureButton != null) {
+            mGestureButton.updateSettings();
         }
     }
 